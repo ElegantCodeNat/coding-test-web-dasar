2db2c3419e9f96a88be340bca0719a9a6267eae8
ElegantCodeNat <natalia.shn94@gmail.com>
2024-09-03 19:31:02 +0700

    5menit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Menjumlahkan data pada array
+
+// input        // result
+// [1,2,3,4,5]     // 15
+// [1,2]           // 3
+// [5]             // 5
+// []              // array kosong
+
+function jumlahArray(arr){
+    let jumlah = 0;
+    for (let i = 0; i < arr.length; i++){
+        jumlah += arr[i]
+    }
+}
